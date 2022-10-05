c88921a3e51749a515f751648ff5f25f08c9be8c
demianchuk-o <demyanchukua@gmail.com>
2022-10-05 05:15:48 +0300

    small changes

--- Lab1/Lab1/Generator.cs
+++ Lab1/Lab1/Generator.cs
@@ -8,10 +8,11 @@ public static class Generator
         {
             int portion = integersAmount switch
             {
-                < 10 * 1024 * 1024 => 1000,
-                (>= 10 * 1024 * 1024) and (< 1024 * 1024 * 1024) => 100000,
-                >= 1024 * 1024 * 1024 => 10000000,
+                < 10*Constants.Mb / sizeof(Int32) => 1000,
+                (>= 10*Constants.Mb / sizeof(Int32)) and (< Constants.Gb / sizeof(Int32)) => 100000,
+                >= Constants.Gb / sizeof(Int32) => 100000000,
             };
+
             Random rng = new Random();
             byte[] buffer = new byte[sizeof(Int32) * portion];
             int data;
@@ -20,23 +21,23 @@ public static class Generator
                 for (int j = 0; j < portion; j++)
                 {
                     data = rng.Next(Int32.MinValue, Int32.MaxValue);
-                    buffer[4 * j - 3] = (byte)data;
-                    buffer[4 * j - 2] = (byte)(data >> 8);
-                    buffer[4 * j - 1] = (byte)(data >> 0x10);
-                    buffer[4 * j] = (byte)(data >> 0x18);
+                    buffer[4 * j] = (byte)data;
+                    buffer[4 * j + 1] = (byte)(data >> 8);
+                    buffer[4 * j + 2] = (byte)(data >> 0x10);
+                    buffer[4 * j + 3] = (byte)(data >> 0x18);
                 }
                 writer.Write(buffer);
             }
             
-            int lastPortionLength = (int)(integersAmount - ((integersAmount / portion) * portion));
-            buffer = new byte[lastPortionLength];
+            int lastPortionLength = (int)(integersAmount - integersAmount / portion * portion);
+            buffer = new byte[sizeof(Int32)*lastPortionLength];
             for (int k = 0; k < lastPortionLength; k++)
             {
                 data = rng.Next(Int32.MinValue, Int32.MaxValue);
-                buffer[4 * k - 3] = (byte)data;
-                buffer[4 * k - 2] = (byte)(data >> 8);
-                buffer[4 * k - 1] = (byte)(data >> 0x10);
-                buffer[4 * k] = (byte)(data >> 0x18);
+                buffer[4 * k] = (byte)data;
+                buffer[4 * k + 1] = (byte)(data >> 8);
+                buffer[4 * k + 2] = (byte)(data >> 0x10);
+                buffer[4 * k + 3] = (byte)(data >> 0x18);
             }
             writer.Write(buffer);
         }
--- Lab1/Lab1/Program.cs
+++ Lab1/Lab1/Program.cs
@@ -1,10 +1,16 @@
-﻿namespace Lab1;
+﻿using System.Diagnostics;
+
+namespace Lab1;
 internal class Program
     {
         public static void Main(string[] args)
         {
             string startFile = "A.dat";
-            
+            long integersToGen = Int64.Parse(Console.ReadLine());
+            Stopwatch sw = Stopwatch.StartNew();
+            Generator.Generate(startFile, integersToGen);
+            sw.Stop();
+            Console.WriteLine($"File generated in {sw.ElapsedMilliseconds}");
         }
         
        
