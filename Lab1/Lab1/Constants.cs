0e38693b8b854038b37eec1dac50cb894c026be8
demianchuk-o <demyanchukua@gmail.com>
2022-10-05 04:51:54 +0300

    created basic constants class

---
+++ Lab1/Lab1/Constants.cs
@@ -0,0 +1,8 @@
+﻿namespace Lab1;
+
+public static class Constants
+{
+    public static readonly int Kb = 1024;
+    public static readonly int Mb = 1048576;
+    public static readonly int Gb = 1073741824;
+}
