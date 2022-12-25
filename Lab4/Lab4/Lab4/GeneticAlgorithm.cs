3f3089b029d3598f86dbb1a8a1f6731fe5149d74
demianchuk-o <demyanchukua@gmail.com>
2022-12-25 02:06:19 +0200

    basic fields for classes

---
+++ Lab4/Lab4/Lab4/GeneticAlgorithm.cs
@@ -0,0 +1,42 @@
+﻿namespace Lab4;
+
+public class GeneticAlgorithm
+{
+    private Store _store;
+    private int[][] _population;
+    private static int _populationSize = 100;
+    private static int _capacity = 150;
+    private static double _mutationProb = 0.05;
+
+
+    public GeneticAlgorithm()
+    {
+        _store = new Store();
+        _population = new int[_populationSize][];
+        int[] randomItems = new int[Store.AMT_OF_ITEMS];
+        for (int i = 0; i < _populationSize; i++)
+        {
+            randomItems[i] = i;
+            _population[i] = new int[_populationSize];
+            for (int j = 0; j < _populationSize; j++)
+                _population[i][j] = 0;
+        }
+        new Random().Shuffle(randomItems);
+
+        for (int i = 0; i < _populationSize; i++)
+        {
+            _population[i][randomItems[i]] = 1;
+        }
+    }
+}
+/*
+ Задача про рюкзак 
+ (місткість P=150, 100 предметів,                                   +
+ цінність предметів від 2 до 10 (випадкова),                        +
+ вага від 1 до 5 (випадкова)),                                      +
+ генетичний алгоритм 
+ (початкова популяція 100 осіб кожна по 1 різному предмету,         +
+ оператор схрещування рівномірний, 
+ мутація з ймовірністю 5% два випадкові гени міняються місцями). 
+ Розробити власний оператор локального покращення.
+ */
---
+++ Lab4/Lab4/Lab4/Store.cs
@@ -0,0 +1,20 @@
+﻿namespace Lab4;
+
+public class Store
+{
+    public static int AMT_OF_ITEMS = 100;
+    public static int VALUE_LOWER = 2;
+    public static int VALUE_UPPER = 10;
+    public static int WEIGHT_LOWER = 1;
+    public static int WEIGHT_UPPER = 5;
+    public readonly Tuple<int, int>[] ITEMS;
+
+    public Store()
+    {
+        Random rng = new Random();
+        for (int i = 0; i < AMT_OF_ITEMS; i++)
+        {
+            ITEMS[i] = new Tuple<int, int>(rng.Next(VALUE_LOWER, VALUE_UPPER), rng.Next(WEIGHT_LOWER, WEIGHT_UPPER));
+        }
+    }
+}
